4a85b4f2194a7d4bfa11cb75165a8687ed370edc
Abdelhamid Ahbane <108112325+abdel-ahbane@users.noreply.github.com>
2026-09-22 17:35:12 +0200

    task 2

---
+++ Types.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+    zahl = 10
+    kommazahl = 10.5
+    text = "hello, world!"
+    wahrheitswert = True
+
+    print("zahl is an " + str(type(zahl)) + " type with value: " + str(zahl))
+    print("kommazahl is an " + str(type(kommazahl)) + " type with value: " + str(kommazahl))
+    print("text is an " + str(type(text)) + " type with value: " + text)
+    print("wahrheitswert is an " + str(type(wahrheitswert)) + " type with value: " + str(wahrheitswert))
